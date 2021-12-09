1c73cce7c5e81601a0cb68755619fe55bd52f3ad
Gabin Guyot <gabin.guyot17@gmail.com>
2021-12-09 10:55:37 +0100

    fix auth

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -2,15 +2,21 @@ const { verifySignUp } = require(appRoot + "/middlewares");
 const controller = require(appRoot + "/controllers/auth-controller");
 
 module.exports = function (app) {
-    app.use(function (req, res, next) {
-        res.header(
-            "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-Type, Accept"
-        );
-        next();
-    });
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Origin", "*");
 
-    app.post("/user/register", verifySignUp.checkDuplicateUsernameOrEmail, controller.signup);
-    app.post("/user/authenticate", controller.signin);
-    // app.put("/user/application/test/test/test/test", controller.stockAppInfo);
+    next();
+  });
+
+  app.post(
+    "/user/register",
+    verifySignUp.checkDuplicateUsernameOrEmail,
+    controller.signup
+  );
+  app.post("/user/authenticate", controller.signin);
+  // app.put("/user/application/test/test/test/test", controller.stockAppInfo);
 };
