55770c3a64cd7f9be8e4d02925d5a93aeb695e45
paul-adrien <plaurent@student.42.fr>
2021-12-09 11:25:10 +0100

    edit socket token

--- front/src/app/_services/websocketService.ts
+++ front/src/app/_services/websocketService.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Subject } from 'rxjs';
@@ -7,44 +6,41 @@ import { io } from 'socket.io-client';
 import { environment } from 'src/environments/environment';
 import { AuthService } from './auth_service';
 
-
 @Injectable()
 export class WebsocketService {
+  // Our socket connection
 
-    // Our socket connection
+  public socket;
+  public token: string;
 
-    public socket;
-    public token: string;
+  constructor(private authenticateService: AuthService) {}
 
-    constructor(authenticateService: AuthService) {
-        this.token = authenticateService.getToken();
-    }
+  ngOnDestroy() {
+    this.socket.removeAllListeners();
+  }
 
-    ngOnDestroy() {
-        this.socket.removeAllListeners();
-    }
+  setupSocketConnection() {
+    this.token = this.authenticateService.getToken();
+    this.socket = io(environment.SOCK_API, {
+      query: { token: this.token },
+    });
+  }
 
-    setupSocketConnection() {
-      this.socket = io(environment.SOCK_API, {
-        query: {token: this.token}
+  listenToServer(connection: string): Observable<any> {
+    return new Observable((subscribe) => {
+      this.socket?.on(connection, (data) => {
+        subscribe.next(data);
       });
-    }
+    });
+  }
 
-    listenToServer(connection: string): Observable<any> {
-        return new Observable((subscribe) => {
-            this.socket?.on(connection, (data) => {
-                subscribe.next(data);
-            });
-        });
-    }
+  emitToServer(connection: string, data: any): void {
+    this.socket?.emit(connection, data);
+  }
 
-    emitToServer(connection: string, data: any): void {
-        this.socket?.emit(connection, data);
+  disconnect() {
+    if (this.socket) {
+      this.socket?.disconnect();
     }
-
-    disconnect() {
-        if (this.socket) {
-            this.socket?.disconnect();
-        }
-    }
+  }
 }
